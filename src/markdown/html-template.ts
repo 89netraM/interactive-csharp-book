c0a30dcd0e09d17dc6bd5b201bdbdd8827d7cf25
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-24 15:30:35 +0200

    Injects script references

--- src/markdown/html-template.ts
+++ src/markdown/html-template.ts
@@ -1,3 +1,13 @@
+const headInjections = [
+	`<script src="_framework/blazor.webassembly.js"></script>`,
+	`<script src="code-component.js"></script>`
+].join("");
+
 export function insertIntoTemplate(markdownHTML: string, template: string): string {
-	return template.replace("<!-- CONTENT -->", markdownHTML);
+	let html = template;
+
+	html = html.replace("<!-- CONTENT -->", markdownHTML);
+	html = html.replace("<!-- HEAD -->", headInjections);
+
+	return html;
 }
